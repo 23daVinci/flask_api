e4d750f3f141395ae0091673873e3ebd70d97bf3
Naman Lazarus <namanlazarus02@gmail.com>
2021-10-03 19:54:52 +0530

    Get and Post methods

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html', name = "Naman")
+
+@app.route('/form-forward', methods = ['GET', 'POST'])
+def form_forward():
+    if request.method == 'POST':
+        return render_template('form_forward.html', query = request.form['details'])
+    else:
+        return jsonify(message = "This is not valid"), 401
 
 @app.route('/api')
 def api():
